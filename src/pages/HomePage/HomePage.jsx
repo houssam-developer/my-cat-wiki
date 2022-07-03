7eac1d2dc03da0c46bf54c3418cd65424511f268
houssam-dev <developpeur.pro.ma@gmail.com>
2022-07-03 04:53:27 +0100

    style: add layout custom masounry skeleton

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -27,6 +27,11 @@ function HomePage() {
 			url: "https://cdn2.thecatapi.com/images/83htMb1AJ.jpg",
 		}
 	]);
+	const [whyHaveCatPictures, setWhyHaveCatPictures] = useState([
+		"https://media.istockphoto.com/photos/european-short-haired-cat-picture-id1072769156?k=20&m=1072769156&s=612x612&w=0&h=k6eFXtE7bpEmR2ns5p3qe_KYh098CVLMz4iKm5OuO6Y=",
+		"https://www.thesprucepets.com/thmb/hThcFCxT20ql0opGe4B8WGICbc4=/1851x1851/smart/filters:no_upscale()/cat-talk-eyes-553942-hero-df606397b6ff47b19f3ab98589c3e2ce.jpg",
+		"https://idsb.tmgrup.com.tr/ly/uploads/images/2021/09/08/thumbs/800x531/142774.jpg"
+	]);
 
 	return (
 		<div className='flex flex-col gap-2'>
@@ -77,6 +82,55 @@ function HomePage() {
 						)}
 					</ul>
 				</div>
+			</section>
+			<section>
+
+				{/* WHY BLOCK */}
+				<div>
+					<h2>Why should you have a cat?</h2>
+					<p>Having a cat around you can actually trigger the release of calming chemicals in your body which lower your stress and anxiety leves</p>
+					<a href="#">
+						<span>READ MORE</span>
+						<MdArrowRightAlt />
+					</a>
+				</div>
+
+				{/* MASOUNRY GALLERY */}
+
+				<div className='container-whyhavecatgrid'>
+					<div className='container-whyhavecatgrid__first'>
+						<div className='min-h-[100%] max-w-[100%] rounded-xl bg-orange-300 flex'>
+							<img className='image-cover rounded-xl' src="https://media.istockphoto.com/photos/european-short-haired-cat-picture-id1072769156?k=20&m=1072769156&s=612x612&w=0&h=k6eFXtE7bpEmR2ns5p3qe_KYh098CVLMz4iKm5OuO6Y=" alt="" />
+						</div>
+					</div>
+					<div className='container-whyhavecatgrid__second'>
+						<div className='bg-neutral-200 min-h-[70%] max-w-[100%] rounded-xl'>sub-second</div>
+					</div>
+					<div className='container-whyhavecatgrid__third'>
+						<div className='bg-blue-200 min-h-[100%] max-w-[75%] flex ml-auto rounded-xl'>sub-third</div>
+					</div>
+				</div>
+
+				{
+				/* <div className='flex items-start'>
+					<div className='flex flex-col w-full border-2 border-orange-300 flex-grow'>
+						<div className='max-w-[300px] bg-slate-300'>
+							<img src="https://media.istockphoto.com/photos/european-short-haired-cat-picture-id1072769156?k=20&m=1072769156&s=612x612&w=0&h=k6eFXtE7bpEmR2ns5p3qe_KYh098CVLMz4iKm5OuO6Y=" alt="" /> 
+						</div>
+						<div className='max-w-[300px] bg-green-400'>
+						 <img src="https://www.thesprucepets.com/thmb/hThcFCxT20ql0opGe4B8WGICbc4=/1851x1851/smart/filters:no_upscale()/cat-talk-eyes-553942-hero-df606397b6ff47b19f3ab98589c3e2ce.jpg" alt="" /> 
+						</div>
+					</div>
+					<div className='w-[100px] bg-purple-400 container-ratio-1-1'>
+					 <img className='child-ratio' src="https://idsb.tmgrup.com.tr/ly/uploads/images/2021/09/08/thumbs/800x531/142774.jpg" alt="" /> 
+					</div>
+				</div> 
+				*/}
+
+				<div>
+					masounry gallery
+				</div>
+
 			</section>
 		</div>
 	)
